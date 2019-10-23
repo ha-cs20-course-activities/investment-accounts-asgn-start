dab9bb56237443f98948c35eb40394dc2f0b0e7c
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2019-10-23 08:42:59 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
-// DS ASGN STARTER TEMPLATE
+// INVESTMENT ACCOUNTS ASSIGNMENT
+
+// Set up Canvas and Graphics Context
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 500;
+
+// Global Variables
+let accounts = [1000, 2000, 3000, 4000, 5000, 6000];
+accounts = [];
+for (let n = 0; n < 100; n++) {
+    accounts.push(Math.random() * 5000);
+}
+let max = 6000; // grade values should be b/t 0 and max
+
+// Main Program Loop
+requestAnimationFrame(draw);
+
+function draw() {
+    // Logic
+    let barWidth = cnv.width / accounts.length;
+
+    // Drawing
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+    // Draw Bar Graph
+    ctx.fillStyle = "orange";
+    ctx.strokeStyle = "grey";
+    for (let i = 0; i < accounts.length; i++) {
+        // Calculate scaled bar height based on cnv.height and canvasMax
+        let barHeight = accounts[i] * (cnv.height / max);
+
+        ctx.fillRect(i * barWidth, cnv.height - barHeight, barWidth, barHeight);
+        ctx.strokeRect(i * barWidth, cnv.height - barHeight, barWidth, barHeight);        
+    }
+
+    // Request another Animation Frame
+    requestAnimationFrame(draw);
+}
+
+// MAIN MENU EVENTS
 
 // DOM Elements
 let outputEl = document.getElementById('output');
@@ -11,11 +52,7 @@ function mainMenu() {
     let selection = document.getElementById('menu').value;
 
     // Take action based on menu selection
-    if (selection == 'display-all') {
-        outputEl.innerHTML = 'Display All';
-    } else if (selection == 'display-range') {
-        outputEl.innerHTML = 'Display Range';
-    } else if (selection == 'generous-donor') {
+    if (selection == 'generous-donor') {
         outputEl.innerHTML = 'Generous Donor';
     } else if (selection == 'hacker-attack') {
         outputEl.innerHTML = 'Hacker Attack';
